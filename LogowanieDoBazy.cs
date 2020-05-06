960270fb56f33cc2df89ecddb4cd301be34f22c6
Marcin Chmielewski <mc@tweaknet.pl>
2020-05-06 20:43:59 +0200

    Final commit, ready for tagging

---
+++ LogowanieDoBazy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace wodomierz
+{
+    public partial class LogowanieDoBazy : Form1
+    {
+        
+        public bool Logowanie()
+        {
+            MessageBox.Show("jesteś zalogowany");
+            return true;
+        }
+
+        public void PokazKtoJestZalogowany()
+        {
+            testToolStripMenuItem.Text = "Zalogowano: " + textBox1.Text + ". Wyloguj.";
+        }
+
+    }
+}
