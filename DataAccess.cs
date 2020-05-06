b16bb7493fa466f602d6fe8cf4b7b5d35d94b38c
Marcin Chmielewski <mc@tweaknet.pl>
2020-05-06 20:42:23 +0200

    So my boss wanted this button ...

---
+++ DataAccess.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace wodomierz
+{
+    public class DataAccess
+    {
+        public List<Klient> GetKlients(string nazwisko)
+        {
+            using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("wodomierz")))
+            {
+                var output = connection.Query<Klient>($"select * from klient where nazwisko = '{nazwisko}' ").ToList();
+                return output;
+            }
+        }
+    }
+}
